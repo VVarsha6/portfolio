78fd13bef1469683aa2db1c2599890ab7d19a3dd
Varsha <vvarsha1020@gmail.com>
2026-01-05 23:43:02 -0800

    moved icons to public

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -50,7 +50,7 @@ export default function ThemeToggle() {
       />
 
       <img
-        src={theme === "dark" ? "src/icons/sun.png" : "src/icons/moon.png"}
+        src={theme === "dark" ? "./icons/sun.png" : "./icons/moon.png"}
         alt=""
         className="h-5 w-5 select-none"
         draggable={false}
