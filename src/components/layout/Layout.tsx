da298a087ad088277752994392946f2de79ee6a9
Varsha <vvarsha1020@gmail.com>
2026-01-06 01:45:18 -0800

    mode icon update

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,9 +12,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         pt-20 sm:pt-24
       "
     >
-      <div className="fixed right-4 bototm-4 z-[999]">
-        <ThemeToggle />
-      </div>
+      <div className="fixed bottom-4 right-4 z-[999] sm:bottom-5 sm:right-5">
+  <ThemeToggle />
+</div>
+
 
       {children}
     </div>
